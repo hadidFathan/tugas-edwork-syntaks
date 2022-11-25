97358690f65282ad9a8405890c1bbe28817a0bc5
hadidfathan <sieghartmchdol@gmail.com>
2022-11-25 16:57:39 +0700

    upload success

---
+++ variable.js
@@ -0,0 +1,16 @@
+//const nama = "edwork";//
+//mengapa error nama, karena tidak ada const nya "nama = "hadid";"//
+//console.log(nama);//
+//maka akan muncul error type assigment //
+
+//const students = {
+   // name:"satria",
+   // class: 2
+//}
+
+//students.faculty = "SI"
+//console.log(students);
+
+const students = ['array','const','object'];
+students.push('play');
+console.log(students);
