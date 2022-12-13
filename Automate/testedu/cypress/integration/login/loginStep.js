fa53c256f4da7fe75c7702e178a8bdee9ab862b2
hadidfathan <sieghartmchdol@gmail.com>
2022-12-13 17:56:00 +0700

    file sucsess

--- Automate/testedu/cypress/integration/login/loginStep.js
+++ Automate/testedu/cypress/integration/login/loginStep.js
@@ -1,13 +1,12 @@
 /// <reference types="cypress"/>
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
-import LoginPage from './loginPage'
 Given('I open login page', () => {
-   LoginPage.visit()
+   cy.visit('http://zero.webappsecurity.com/login.html')
 })
 When('I submit login', () => {
-   LoginPage.fillUsername('username')
-   LoginPage.fillPassword('password')
-   LoginPage.signIn()
+   cy.get('#user_login').type('username')
+   cy.get('#user_password').type('password')
+   cy.contains('Sign in').click()
 })
 Then('I should see homepage', () => {
    cy.get('#account_summary_tab').should('be.visible')
